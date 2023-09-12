0149f47e9d361f86e8041a2da524a9cffd9621d5
Source Academy User <No public email provided>
2023-09-12 11:33:05 +0800

    Changes made from Source Academy

--- studio5.js
+++ studio5.js
@@ -1,6 +1,6 @@
 // studio 5 in class sheet
 
-// returns a list containing all elements of odd index
+// Q1: returns a list containing all elements of odd index
 function every_second(items) {
     return is_null(items) || length(items) === 1
             ? null
@@ -9,18 +9,19 @@ function every_second(items) {
 
 every_second(list("a", "x", "b", "y", "c", "z", "d"));
 
-
+// Q2: helper function that returns sum of list
 function sums_of_list(items) {
     return is_null(items) 
             ? 0
             : list_ref(items, 0) + sums_of_list(tail(items));
 }
 
+// Q2: returns list containing two elements
+// 
 function sums(items) {
     const odds = every_second(items);
     const evens = pair(list_ref(items, 0), every_second(tail(items)));
     return list(sums_of_list(evens), sums_of_list(odds));
-
 }
 
-//sums(list(1, 2, 3, 4, 5, 6, 7));
+sums(list(1, 2, 3, 4, 5, 6, 7));
