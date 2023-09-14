f959cbb7ec96666ed0696f1088bef219b173f730
Source Academy User <No public email provided>
2023-09-14 14:35:33 +0800

    Changes made from Source Academy

--- reflection5.js
+++ reflection5.js
@@ -1,21 +1,10 @@
 // Q1
 function flatten_list(xs) {
-    return is_null(xs)
-            ? null
-            : is_list(head(xs))
-            ? append(flatten_list(head(xs)), flatten_list(tail(xs)))
-            : pair(head(xs), flatten_list(tail(xs)));
-}
-
-function flatten_2(xs) {
-    return accumulate(((x, y) => is_list(x)
-                            ? append(x, y)
-                            : (x => list(x))(x)),
-                        null, xs);
+    return accumulate(append, null, xs);
 }
 
 const LoL = list(list(1, 2), list(3, 4, 5, 6), null, list(7, 8, 9));
-display_list(flatten_2(LoL));
+display_list(flatten_list(LoL));
 // Returns list(1, 2, 3, 4, 5, 6, 7, 8, 9)
 
 
@@ -37,6 +26,14 @@ tree_sum(my_tree);
 
 /*
 // Q3
+function flatten_list(xs) {
+    return is_null(xs)
+            ? null
+            : is_list(head(xs))
+            ? append(flatten_list(head(xs)), flatten_list(tail(xs)))
+            : pair(head(xs), flatten_list(tail(xs)));
+}
+
 function accumulate_tree(f, op, initial, tree) {
     return accumulate(((x, y) => is_list(x)
                                 ? op(x, y)
@@ -45,7 +42,7 @@ function accumulate_tree(f, op, initial, tree) {
 }
 
 function flatten(tree) {
-    return accumulate_tree(x => list(x), append, null , tree);
+    return accumulate_tree(x => list(x), append, null, tree);
 }
 
 const LoL = list(list(1, 2), list(3, 4, 5, 6), null, list(7, 8, 9));
